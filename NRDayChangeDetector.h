e08cc174a7531f45bed724c34cd042beb7528bdd
Nikolai Ruhe <git@nikolai-ruhe.de>
2014-08-28 13:44:23 +0200

    Add some documentation.

--- NRDayChangeDetector.h
+++ NRDayChangeDetector.h
@@ -3,6 +3,21 @@
 
 extern NSString *NRCurrentCalendarDayDidChange;
 
+
+// NRDayChangeDetector detects changes of the current calendar day and sends a notification.
+//
+// It uses the system's current calendar (as selected by the user). NRDayChangeDetector
+// is smart enough to detect changes because of daylight saving time, setting the system
+// clock, changing the timezone, and after system sleep.
+//
+// To use it in your app just start it and register for its single notification:
+//
+//	  [NRDayChangeDetector startDetectingDayChanges];
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(dayDidChange:)
+//                                                 name:NRCurrentCalendarDayDidChange
+//                                               object:nil]
+
 @interface NRDayChangeDetector : NSObject
 
 + (void)startDetectingDayChanges;
